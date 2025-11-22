65b4335301d4a1a5654772f5e63c8cdaa10ecd56
Saksham Maurya <sakshammaurya3232@gmail.com>
2025-11-22 11:24:07 +0530

    Add debug static file serving to URL configurations

--- samkart/urls.py
+++ samkart/urls.py
@@ -21,13 +21,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     # path('', include(('customer.urls', 'customer'), namespace='customer')),
     path('', include('customer.urls')),
     path('services/', include('seller.urls')),
 ]
 
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
